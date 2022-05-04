493953e7df919538aa06bdbe0d3ff7f89d0c9c67
mednologic <mednologic@gmail.com>
2022-05-04 18:33:43 +0200

    "Exercise 2 UD20"

---
+++ t2/src/main/java/com/dou/ud20/t2/App.java
@@ -0,0 +1,13 @@
+package com.dou.ud20.t2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
